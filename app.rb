4ce7c105e241d485a12549baa77d4a9d369af95b
JoeFPollock <josephfpollock@gmail.com>
2018-06-09 17:11:08 +0100

    Added controllers/members_controller.rb, views/members/index.erb, and views/members/show.erb

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-#require_relative('controllers/members_controller')
+require_relative('controllers/members_controller')
 #require_relative('controllers/sessions_controller')
 #require_relative('controllers/bookings_controller')
 
